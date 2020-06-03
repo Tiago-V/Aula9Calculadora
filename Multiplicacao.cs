4136849497a191d51eec8816b72995585fc24525
unknown <tiagocvalim@gmail.com>
2020-06-03 10:27:04 -0300

    Multiplicação feita

---
+++ Multiplicacao.cs
@@ -0,0 +1,11 @@
+namespace Aula_09_Calculadora
+{
+    public class Multiplicacao : Calculadora
+    {
+        public double multiplica(){
+
+            return (valor1 * valor2);
+
+        }
+    }
+}
